e65a1133d55bed68d5727a70a70c6811db043f96
colci <72317481+colci@users.noreply.github.com>
2021-05-21 16:39:37 +0300

    Delete lession-2 directory

--- lession-2/task_3.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Введите"))
-res = 0
-while a > 0:
-    res *= 10
-    res += a % 10
-    a //= 10
-print(res)
--- lession-2/task_5.py
+++
@@ -1,3 +0,0 @@
-print(f"Код | Символ")
-for n in range(32, 127):
-    print(f"{n} - {chr(n)}")
